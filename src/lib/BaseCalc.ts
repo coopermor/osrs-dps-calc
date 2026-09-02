90d9c9e11f01aed76e121c37c496c5e4a94b7966
coopermor <coopermorris96@yahoo.com>
2026-09-02 09:57:00 -0700

    Add Seeking broad arrows to isWearingLeafBladedWeapon

--- src/lib/BaseCalc.ts
+++ src/lib/BaseCalc.ts
@@ -535,6 +535,7 @@ export default class BaseCalc {
         'Broad arrows',
         'Broad bolts',
         'Amethyst broad bolts',
+        'Seeking broad arrows',
       ])
       && this.player.style.type === 'ranged'
     ) {
